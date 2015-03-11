7d03dab211604beb29892f3786e075e5bab2bd1e
0x7F800000 <vw6j0+5qe874nq1v6xg@sharklasers.com>
2015-03-11 14:33:32 -0400

    Update common.hpp

--- util/common.hpp
+++ util/common.hpp
@@ -6,36 +6,6 @@ namespace util
 {
 namespace common
 {
-	/*
-		integer primitive types
-	*/
-	using uint8		=	unsigned char;
-	using int8 		=	signed char;
-
-	using uint16	=	unsigned short;
-	using int16 	=	signed short;
-
-	using uint32 	=	unsigned int;
-	using int32 	=	signed int;
-
-	using uint64 	=	unsigned __int64;//unsigned long long;
-	using int64 	=	__int64;//signed long long;
-
-	/*
-		sanity checks
-	*/
-	static_assert(	sizeof(uint8)	==	1	&&	sizeof(int8)	==	1, "sizeof int8/uint8 ought to be one byte...");
-	static_assert(	sizeof(uint16)	==	2	&&	sizeof(int16)	==	2, "sizeof int16/uint16 should be two bytes...");
-	static_assert(	sizeof(uint32)	==	4	&&	sizeof(int32)	==	4, "sizeof int32/uint32 should be four bytes...");
-	static_assert(	sizeof(uint64)	==	8	&&	sizeof(int64)	==	8, "sizeof int64/uint64 should be eight bytes...");
-
-	/*
-		real number types
-	*/
-	using real32	=	float;
-	using real64	=	double;
-	using real128	=	long double;
-
 	/**
 	 * like sizeof, but for bits instead of bytes
 	 * intended for use with basic types
@@ -81,8 +51,6 @@ namespace common
 		"meta::popcnt is returning inaccurate results..."
 	);
 
-	static constexpr ptrdiff_t 	none	=  -1;
-	static constexpr size_t 	unone	= (none);
 }
 }
 };
