10aa9d9fa12614d775c859bb1a49e0e17f25f659
0x7F800000 <vw6j0+5qe874nq1v6xg@sharklasers.com>
2015-03-11 14:29:55 -0400

    Need to split util/common.hpp

--- util/common.hpp
+++ util/common.hpp
@@ -35,6 +35,54 @@ namespace common
 	using real32	=	float;
 	using real64	=	double;
 	using real128	=	long double;
+
+	/**
+	 * like sizeof, but for bits instead of bytes
+	 * intended for use with basic types
+	 */
+	template<typename T> constexpr size_t bitSizeof = 	sizeof(T) * 8;
+
+	static_assert
+	(
+			bitSizeof<char>			==	8
+		&&	bitSizeof<short>		==	16
+		&&	bitSizeof<int>			==	32
+		&&	bitSizeof<long long>	==	64,
+		"Apparently bitSizeof is fuckt. Pretty cray."
+	);
+	/**
+	 * popcnt
+	 *
+	 * counts the number of bits set
+	 */
+	template<typename T> constexpr
+	size_t popcnt(const T inputVal)
+	{
+		static_assert(is_integral(T), "popcnt requires an integral type.");
+
+		/*
+			signed shift would produce inaccurate results
+		*/
+		make_unsigned(T) val = static_cast<make_unsigned(T)>(inputVal);
+		size_t cnt = 0;
+
+		for(size_t i = 0; i < bitSizeof<T>; ++i )
+		{
+			if(val & 1 != 0)
+				++cnt;
+			val >>= 1;
+		}
+		return cnt;
+	}
+
+	static_assert
+	(
+		popcnt<int32>(3) == 2,
+		"meta::popcnt is returning inaccurate results..."
+	);
+
+	static constexpr ptrdiff_t 	none	=  -1;
+	static constexpr size_t 	unone	= (none);
 }
 }
 };
