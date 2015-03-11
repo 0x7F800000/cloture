07b326ed0af78cd5a1bea727491ffd008d1f0e52
0x7F800000 <vw6j0+5qe874nq1v6xg@sharklasers.com>
2015-03-11 19:16:04 -0400

    Added constexpr math functions

--- util/basic_types.hpp
+++ util/basic_types.hpp
@@ -3,33 +3,58 @@
 namespace cloture {
 namespace util    {
 namespace common  {
-  /*
-  integer primitive types
-  */
-  using uint8 = unsigned char;
-  using int8 = signed char;
-  using uint16 = unsigned short;
-  using int16 = signed short;
-  using uint32 = unsigned int;
-  using int32 = signed int;
-  using uint64 = unsigned __int64;//unsigned long long;
-  using int64 = __int64;//signed long long;
-  /*
-  sanity checks
-  */
-  static_assert( sizeof(uint8) == 1	&& sizeof(int8) == 1, "sizeof int8/uint8 ought to be one byte...");
-  static_assert( sizeof(uint16) == 2	&& sizeof(int16) == 2, "sizeof int16/uint16 should be two bytes...");
-  static_assert( sizeof(uint32) == 4	&& sizeof(int32) == 4, "sizeof int32/uint32 should be four bytes...");
-  static_assert( sizeof(uint64) == 8	&& sizeof(int64) == 8, "sizeof int64/uint64 should be eight bytes...");
-  /*
-  real number types
-  */
-  using real32 = float;
-  using real64 = double;
-  using real128 = long double;
+	/*
+	integer primitive types
+	*/
+	using uint8 = unsigned char;
+	using int8 = signed char;
+	using uint16 = unsigned short;
+	using int16 = signed short;
+	using uint32 = unsigned int;
+	using int32 = signed int;
+	using uint64 = unsigned __int64;//unsigned long long;
+	using int64 = __int64;//signed long long;
+
+	#if defined(__clang__) || defined(__GNUC__) || defined(__INTEL_COMPILER)
+		#define	compilerSupportsInt128	1
+	#else
+		#define	compilerSupportsInt128	0
+	#endif
+
+	#if compilerSupportsInt128 && defined(__x86_64__)
+		using int128	= __int128;
+		using uint128	= unsigned __int128;
+	#else
+		/*
+		 * include some header here containing a class to emulate __int128
+		 */
+	#endif
+
+
+	/*
+	sanity checks
+	*/
+	static_assert( sizeof(uint8) == 1	&& sizeof(int8) == 1, "sizeof int8/uint8 ought to be one byte...");
+	static_assert( sizeof(uint16) == 2	&& sizeof(int16) == 2, "sizeof int16/uint16 should be two bytes...");
+	static_assert( sizeof(uint32) == 4	&& sizeof(int32) == 4, "sizeof int32/uint32 should be four bytes...");
+	static_assert( sizeof(uint64) == 8	&& sizeof(int64) == 8, "sizeof int64/uint64 should be eight bytes...");
+	/*
+	real number types
+	*/
+	using real32 = float;
+	using real64 = double;
+	using real80 = long double;
+
 
 	static constexpr ptrdiff_t 	none	=  -1;
 	static constexpr size_t 	unone	= (none);
+
+	template<typename T> static constexpr T maxValue 	= 0;
+
+	template<> static constexpr real64 	maxValue<real64> 	= 1.79769e+308;
+	template<> static constexpr real32 	maxValue<real32> 	= 3.40282e+38f;
+	template<> static constexpr real80 	maxValue<real80>	= (sizeof(real80) > sizeof(double) ? 1.18973e+4932L : maxValue<real64>);
+
 }//namespace common
 }//namespace util
 }//namespace cloture
--- util/ctfe.hpp
+++ util/ctfe.hpp
@@ -590,6 +590,122 @@ namespace ctfe
 	 */
 	namespace math
 	{
+		using arithDefault = double;
+
+		using namespace cloture::util::common;
+		constexpr uint32 SngFwd(const uint32 sign, const uint32 exponent, const float mt)
+		{
+			const uint32 b = static_cast<uint32>(8388608.0 * static_cast<double>(mt) + .5);
+			return
+			(0xFF & b)
+			|
+			(0xFF00 & b)
+			|
+			((0x7F0000 & b)
+			|
+			((exponent & 1) << 23))
+			|
+			((sign << 31) | ((exponent >> 1) << 24));
+		}
+
+		/*
+		 * ctfe::math::abs
+		 * calculates the absolute value of x
+		 * for integral and floating point types.
+		 * there is no fabs
+		 */
+		template<typename T>
+		static constexpr T abs(const T x)
+		{
+			constexpr T ZERO = static_cast<T>(0);
+			if(x < ZERO)
+				return -x;
+			return x;
+		}
+
+		/**
+		 * ctfe::math::sqrt
+		 * calculates the square root of x (dumbass)
+		 */
+		template<typename T>
+		static constexpr T sqrt(const T x)
+		{
+			constexpr T TWO 	= static_cast<T>(2);
+			constexpr T ZERO = static_cast<T>(0);
+			T result = x / TWO;
+			if(result < ZERO)
+				return ZERO;
+
+			#define	__SQRT_LOOP_CONDITION()	static_cast<double>(\
+			abs( result - (x / result)) / result	\
+	) > 0.000000000000001
+
+			#define		__SQRT_ONE_ITERATION()	\
+				if(!(__SQRT_LOOP_CONDITION())) break; else result = (result + (x / result)) / TWO
+
+			while(	__SQRT_LOOP_CONDITION()	)
+			{
+				result = (result + (x / result)) / TWO;
+				__SQRT_ONE_ITERATION();
+				__SQRT_ONE_ITERATION();
+			}
+			return result;
+		}
+
+		/**
+		 * regression tests for sqrt
+		 */
+		static_assert(
+		sqrt<double>(536.0) == 23.15167380558045
+		&&
+		sqrt<double>(8000.0 * 1336.0) == 3269.250678672408
+		&&
+		sqrt<double>(1.79769e+308) == 1.3407796239501852e+154);
+
+		/**
+		 * 	ctfe::math::hypot
+		 * 	calculates hypotenuse of a right-angled triangle with legs x and y
+		 */
+		template<typename T>
+		static constexpr T hypot(const T x, const T y)
+		{
+			return sqrt<T>(x*x+y*y);
+		}
+
+		static_assert(
+		hypot<double>(50.0, 119.0) == 129.07749610214788);
+
+		template<typename T>
+		T ceil(const T x)
+		{
+			using iType = int64;
+
+			constexpr T ZERO	= static_cast<T>(0);
+			constexpr T ONE		= static_cast<T>(1);
+
+			const iType xInt = static_cast<iType>(x);
+			if(x < ZERO)
+				return static_cast<T>(xInt);
+			return static_cast<T>(xInt + ONE);
+
+		}
+		template<typename T>
+		T cbrt(const T x)
+		{
+			constexpr T FOUR = static_cast<T>(4);
+			constexpr T THREE = static_cast<T>(3);
+			T result = x / FOUR;
+
+			#define __CBRT_LOOP_CONDITION()		\
+				abs(result - (x / result / result)) / result > 0.00000000000001
+
+			while(__CBRT_LOOP_CONDITION())
+			{
+				result = (result + (x / result / result) + result) / THREE;
+			}
+			return result;
+		}
+		constexpr __int128 ruhroh = 0;
 
 	}
 
